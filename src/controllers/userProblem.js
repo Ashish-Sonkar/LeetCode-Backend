6a6eb09c8433d5e227aa952d53cfdd8b94635c5b
Ashish-Sonkar <ashishsonkar3452@gmail.com>
2026-09-13 15:48:43 +0530

    Update LeetCode backend

---
+++ src/controllers/userProblem.js
@@ -0,0 +1,15 @@
+
+
+const createProblem=async (req,res)=>{
+    const {title,description,difficulty,tags,visibleTestCases,hiddenTestCases,startCode,referenceSolution,problemCreator} = req.body
+
+    try{
+
+        for(const {language,completeCode} of referenceSolution){
+
+        }
+
+    }catch(err){
+
+    }
+}
--- src/routes/problemCreator.js
+++ src/routes/problemCreator.js
@@ -1,15 +1,16 @@
 const express=require("express")
 const problemRouter=express.Router()
+const adminMiddleware=require("../middleware/adminMiddleware.js")
 
 
 //create but need of admin access
-problemRouter.post("/create",problemCreate)
+problemRouter.post("/create",adminMiddleware,createProblem)
 //update but need of admin access
-problemRouter.patch("/:id",problemUpdate)
+problemRouter.patch("/:id",updateProblem)
 //delete but need of admin access
-problemRouter.delete("/:id",problemDelete)
+problemRouter.delete("/:id",deleteProblem)
 
 //fetch
-problemRouter.get("/:id",problemFetch)
+problemRouter.get("/:id",getProblemById)
 problemRouter.get("/",getAllProblem)
 problemRouter.get("/user",solvedProblem)
